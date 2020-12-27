aaaa1f3a5ccd002679ede912dbf6203a1efc977b
vishvapatel2529 <email_2529vishva@gmail.com>
2020-12-27 16:00:58 -0600

    project

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnclosemodal = document.querySelector('.close-modal');
 const btnopenmodal = document.querySelectorAll('.show-modal');
 
-const openmodal = function () {
+function openmodal() {
   console.log('button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-};
+}
 
 for (let i = 0; i < btnopenmodal.length; i++)
   btnopenmodal[i].addEventListener('click', openmodal);
 
-const closemodal = function () {
+function closemodal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-};
+}
 
 btnclosemodal.addEventListener('click', closemodal);
 overlay.addEventListener('click', closemodal);
